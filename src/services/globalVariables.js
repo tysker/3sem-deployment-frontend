6f8a9a5a8a8f17ccd738b69d25fe3b8cbfe9c36a
tysker <joergoertel@proton.me>
2024-05-16 08:41:10 +0200

    deployment

--- src/services/globalVariables.js
+++ src/services/globalVariables.js
@@ -1,7 +1,7 @@
 const deployed = import.meta.env.PROD
 console.log("deployed", deployed)
 const BASE_URL_DEV = "http://localhost:7070/api/v1"
-const BASE_URL_PROD = "https://web.joergoertel.dk/api/v1"
+const BASE_URL_PROD = "https://api.joergoertel.dk/api/v1"
 export const BASE_URL = deployed ? BASE_URL_PROD : BASE_URL_DEV
 export const INIT_USER_LOGIN = {username: "", roles: ["USER"]}
 export const INIT_USER_REGISTER = {username: "", password: "", roles: ["USER"]}
